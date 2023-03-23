7c4a3768f0cf22fa9b481be7c59f2839a3065ddc
JoeHinton <hintonmjoseph@gmail.com>
2023-03-22 23:10:30 -0500

    create block07

---
+++ block07/script.js
@@ -0,0 +1,17 @@
+/* Create 3 variables, each with 3 arithmetic operators */ 
+/* Display a concatenated print of the 3 variables on button press (alert) */
+/* Change image of closed vault to open vault on button press */
+
+/* First number of the combo */
+const firstNumber = 10 * 2/ 4 + 5;
+
+/* Second number of the combo */
+const secondNumber = (100 + 20) /3;
+
+/* Third number of the combo */
+const thirdNumber = (secondNumber/firstNumber*9+3)
+
+/* The pop-up alert with message and combo */
+alert ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:" + `\n` + `\n` + firstNumber + "-" + secondNumber + "-" + thirdNumber);
+
+
